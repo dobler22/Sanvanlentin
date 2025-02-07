6317d1b3c39bd0a3eb0a7df6d7f02e50819f7bb0
dobler22 <jordirr22@gmail.com>
2025-02-06 19:52:10 -0500

    correcion

--- script.js
+++ script.js
@@ -1,7 +1,7 @@
 document.getElementById('yesButton').addEventListener('click', function() {
-    document.getElementById('responseMessage').textContent = "¡Yay! ¡Eres el/la mejor! 💖";
+    document.getElementById('responseMessage').textContent = "Muejejeje ya lo sabia cuando te vea te beso :3 💖";
 });
 
 document.getElementById('noButton').addEventListener('click', function() {
-    document.getElementById('responseMessage').textContent = "¡Oh no! 😢 Pero seguiré intentándolo...";
+    document.getElementById('responseMessage').textContent = "¡Oh no! 😢 Porque no si sabes que yo te amooo mucho :C...";
 });
